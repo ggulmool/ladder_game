690c7113674cda49bc7a9c08cfa373d88e962de0
GGuLMooL <kimny86@gmail.com>
2017-12-23 18:46:25 +0900

    step1. 일단 구현해본 사다리 로직

--- src/ladder/Ladder.java
+++ src/ladder/Ladder.java
@@ -2,4 +2,33 @@ package ladder;
 
 public class Ladder {
 
+  private int[] row;
+
+  public Ladder(int noOfPerson) {
+    row = new int[noOfPerson];
+  }
+
+  public void drawLine(int startPosition) {
+    row[startPosition] = 1;
+    row[startPosition + 1] = 1;
+  }
+
+  public int run(int nthOfPerson) {
+    // 1 1 0
+    if (nthOfPerson - 1 <= 0 && row[nthOfPerson - 1] == 1) {
+      return nthOfPerson + 1;
+    }
+
+    if (row[nthOfPerson - 1] == 1) {
+      if (row[nthOfPerson - 2] == 1) {
+        return nthOfPerson - 1;
+      }
+
+      if (row[nthOfPerson] == 1) {
+        return nthOfPerson + 1;
+      }
+    }
+
+    return nthOfPerson;
+  }
 }
--- test/ladder/LadderTest.java
+++ test/ladder/LadderTest.java
@@ -1,5 +1,53 @@
 package ladder;
 
-public class LadderTest {
+import junit.framework.TestCase;
 
+public class LadderTest extends TestCase {
+
+  public void testRunWhenNoLine() {
+    int noOfPerson = 1;
+    Ladder ladder = new Ladder(1);
+    int target = ladder.run(noOfPerson);
+    assertEquals(noOfPerson, target);
+
+    target = ladder.run(2);
+    assertEquals(2, target);
+  }
+
+  public void testRunWhenTwoByOne1Line() {
+    // 1 1
+    Ladder ladder = new Ladder(2);
+    ladder.drawLine(0);
+    int target = ladder.run(1);
+    assertEquals(2, target);
+  }
+
+  public void testRunWhenThreeByOne1Line() {
+    // 1 1 0
+    Ladder ladder = new Ladder(3);
+    ladder.drawLine(0);
+    int target = ladder.run(1);
+    assertEquals(2, target);
+
+    target = ladder.run(2);
+    assertEquals(1, target);
+
+    target = ladder.run(3);
+    assertEquals(3, target);
+
+  }
+
+  public void testRunWhenThreeByOne1Line2() {
+    // 0 1 1
+    Ladder ladder = new Ladder(3);
+    ladder.drawLine(1);
+    int target = ladder.run(1);
+    assertEquals(1, target);
+
+    target = ladder.run(2);
+    assertEquals(3, target);
+
+    target = ladder.run(3);
+    assertEquals(2, target);
+  }
 }
